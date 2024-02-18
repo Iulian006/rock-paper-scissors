ba93a8fb9c3f63ca259864d8977d95c0e9625277
Iulian006 <ciuli06ciuli@gmail.com>
2024-02-18 13:56:04 +0200

    added UI

--- script.js
+++ script.js
@@ -3,49 +3,48 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random() * 3)];
 }
 
-function capitalize(string) {
-    return string.slice(0,1).toUpperCase()+string.slice(1).toLowerCase();
-  }
-
 function playRound(playerSelection, computerSelection) {
     const player = playerSelection;
     const bot = computerSelection;
-    if (player==bot){
-        return "It's a tie!";
-    }
-    else if(player=='Rock'){
-        if( bot=='Paper')
-            return "You Lose! Paper beats Rock";
-        else
-            return "You Win! Rock beats Scissors";
-    }
-    else if(player=='Paper'){
-        if( bot=='Scissors')
-            return "You Lose! Scissors beats Paper";
-        else
-            return "You Win! Paper beats Rock";
+    if (playerSelection === computerSelection) {
+        return 0;
+    } 
+    else if((playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper") ||
+        (playerSelection === "Paper" && computerSelection === "Rock")) {
+            return 1;
     }
     else{
-        if(bot=='Rock')
-            return "You Lose! Rock beats Scissors";
-        else
-            return "You Win! Scissors beats Paper";
+        return -1;
     }
   }
   
-  function playGame(count){
-    for(let i = 1; i <= count; i++){
-        const choices = ["Rock", "Paper", "Scissors"];
-        const playerSelection = capitalize(prompt("Choose your action: "));
-        if (choices.includes(playerSelection)){
-            const computerSelection = getComputerChoice();
-            console.log(playRound(playerSelection, computerSelection));
+  function playGame(action){
+    const playerSelection = action;
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection)
+    return { result, computerSelection };
+  }
+
+  const btns = document.querySelectorAll('button');
+  const output = document.querySelector('.output');
+  const botChoice = document.querySelector('.bot');
+  const score = document.querySelector('.score');
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        action = btn.innerHTML;
+        const { result, computerSelection } = playGame(action);
+        botChoice.textContent = 'Bot chose: ' + computerSelection;
+        score.textContent = parseInt(score.textContent) + result;
+        if(result === 1){
+            output.textContent = 'You win!';
+        }
+        else if(result === -1){
+            output.textContent = 'You lost!';
         }
         else{
-            console.log("Invalid Input");
-            i--;
+            output.textContent = "It's a tie!";
         }
-    }
-  }
-  let rounds = parseInt(prompt("How many rounds do you want to play?: "));
-  playGame(rounds);
+    })
+  })
